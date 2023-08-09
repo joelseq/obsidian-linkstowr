b4b3a99f5b732fc36a97fd7c527b034ab936f612
Joel Sequeira <joelseq96@gmail.com>
2023-08-09 00:15:41 -0400

    Change API url to point to fly.io deployment

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -13,7 +13,7 @@ if you want to view the source, please visit the github repository of this plugi
 const prod = process.argv[2] === 'production';
 
 const API_URL = prod
-  ? '"https://linkshelf.shuttleapp.rs"'
+  ? '"https://linkshelf.fly.dev"'
   : '"http://localhost:8000"';
 
 const context = await esbuild.context({
